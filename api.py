dac218b34473963e21216160db20c433712511a6
rchc44 <rchc44@gmail.com>
2022-03-15 13:45:58 -0400

    cleaned up comments

--- api.py
+++ api.py
@@ -24,18 +24,6 @@ auth=firebase.auth()
 
 app=FastAPI()
 
-students={
-    1:{
-        "name":"john",
-        "age":50,
-        "year":"year 12"
-    }    
-}
-
-class Student(BaseModel):
-    name:str
-    age:int
-    year:str
 
 class UserInfo(BaseModel):
     email:str
@@ -48,127 +36,87 @@ class UpdateStudent(BaseModel):
     year: Optional[str]=None
 
 
+class CreateStudent(BaseModel):
+    birthday:str
+    emergencyContact:str
+    firstName:str
+    grade:int
+    lastName:str
+    school:str
+    username:str
+    
 
 
-@app.get("/") # base url, google.com
-def index(): # any name for function
-    return {"name":"first Data"}
+@app.get("/")
+def index():
+    return {"name":"Test Data"}
 
 
 @app.get("/students")
 def getStudents():
     data=db.child("students").get()
-    print(data)
-    data=db.child("students").get()
-    #print(data)
-    bob=[]
-    for datum in data.each():
-        print(datum.val())  
-        bob.append(datum.val())
-    #return bob
     return data
-    return {"data":"rr"}
+    
+    '''
+    dataCleaned=[]
+    
+    for datum in data.each():
+        dataCleaned.append(datum.val())
+    return dataCleaned
+    '''
 
-@app.get("/students/{studentId}")
-def getStudent():
-    return {}
+
+@app.get("/students/{username}") # get by id, username, email
+def getStudent(username:str):
+    data=db.child("students").order_by_child("username").equal_to(username).get()
+    returnVal={}
+    
+    for datum in data.each():
+        returnVal["data"]=datum.val()
+        
+    if not returnVal:
+        returnVal["message"]="username not found"        
+    return returnVal
+    
 
 
 # signup
 @app.post("/signup")
-def signup(email:str,pw:str):
+def signup(userInfo:UserInfo,createStudent:CreateStudent):
     try:
-        user = auth.create_user_with_email_and_password(email, pw)
+        user = auth.create_user_with_email_and_password(userInfo.email, userInfo.pw)
+        
+        dataStudent=createStudent.dict()
+        dataStudent["email"]=userInfo.email
+        db.child("students").push(dataStudent)
         return user
-    except:
-        print("could not sign up")
-        return {"Error":"Could not sign up"} 
+    except Exception as e:
+        e=e.strerror
+        e=json.loads(e)
+        return {"code":e['error']['code'],"message":e['error']['message']}
 
-# login, post https://stackoverflow.com/questions/43965316/for-login-get-or-post
+
+
+# login
 @app.post("/login")
 def login(userInfo:UserInfo):
     try:
         user = auth.sign_in_with_email_and_password(userInfo.email,userInfo.pw)
-        return user
+        return user # important properties: refreshToken,idToken
     except Exception as e:
-        print("could not sign in")
-        return {"Error":e}
-        return {"Error":"Could not sign in"}
+        e=e.strerror
+        e=json.loads(e)
+        return {"code":e['error']['code'],"message":e['error']['message']}
 
-# logout, post https://stackoverflow.com/questions/3521290/logout-get-or-post?noredirect=1&lq=1
+
+# logout 
 @app.post("/logout")
 def logout():
+    #auth.current_user = None
     return {}
 
 
-data=db.child("students").get()
-#print(data)
-for datum in data.each():
-    print(datum.val())
 
-
-'''
-try:
-    user = auth.sign_in_with_email_and_password("errhg","dgr")
-    print("success")
-except Exception as e:
-    e=str(e)
-    print("could not sign in")
-    print(e) 
-'''
-'''
-
-	# students
-
-
-GET /students/{studentId}
-	get student info
-POST /students
-	create new student
-PUT /students/{studentId}
-	update existing student info
-DELETE /students/{studentId}
-	delete existing student
-
-
-	# teachers 
-	
-	
-GET /teachers/{teacherId}
-	get teacher info
-POST /teachers
-	create new teacher
-PUT /teachers/{teacherId}
-	update existing teacher info
-DELETE /users/{userId}
-	delete existing teacher
-
-
-	# tests
-
-
-GET /tests/{testId}
-	get specific test from db
-POST /tests
-	upload new test
-PUT /tests/{testId}
-	update existing test
-DELETE /tests/{testId}
-	delete existing test
-
-
-	# grades
-GET /tests/{testId}/grades/{gradeId}
-	get grade for specific test
-POST /tests/{testId}/grades
-	upload grade for existing test
-PUT /tests/{testId}/grades/{gradeId}
-	update grade for existing test
-DELETE /tests/{testId}/grades/{gradeId}
-	delete existing grade
-	
-
-'''
 
 
 
